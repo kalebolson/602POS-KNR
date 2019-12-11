30971db7d7698e294f8afbefab151e5e363cf22c
nicole.burns <nicole.burns@US-BOS-LAP-032.dotmatics.local>
2019-12-10 20:56:43 -0600

    testing

--- POSSystem/src/POSDriver.java
+++ POSSystem/src/POSDriver.java
@@ -5,13 +5,9 @@ public class POSDriver {
 		Store a = new Store(); 
 		Register r = new Register(1.00, a); 
 		
-		System.out.println(r.inventoryReport());
+		System.out.println(r.CashierReportZ());
 		System.out.println(); 
-		//this should print apple information
-		System.out.println(r.productReport(1));
 		
-		//this should print banana information.. if not, will print "nope"
-		System.out.println(r.productReport(2));
 	}
 
 
