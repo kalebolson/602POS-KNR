16f7cb3753d2fd615844e2a844ff902c13b3c616
nicole.burns <nicole.burns@US-BOS-LAP-032.dotmatics.local>
2019-12-11 09:08:16 -0600

    driver tests

--- POSSystem/src/POSDriver.java
+++ POSSystem/src/POSDriver.java
@@ -5,7 +5,7 @@ public class POSDriver {
 		Store a = new Store(); 
 		Register r = new Register(1.00, a); 
 
-		System.out.println(r.orderingNeedsCondensedReport());
+		System.out.println("test");
 	}
 
 
